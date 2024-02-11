8a9459781b5e2754d5e3f741e54a0e50fea7cb3f
RaiDenNsg <159679368+RaiDenNsg@users.noreply.github.com>
2024-02-11 11:44:32 +0545

    fiels for c

---
+++ aryan.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+ {
+   int n[10];
+   int i,max;
+    printf("Enter a number ");
+    	for (i=0;i<10;i++)
+    	{
+    		scanf("%d",&n[i]);
+		}
+    
+    max=n[0];
+        printf("\nComparing Value");
+    for(i=1;i<10;i++)
+    {
+    	if(max<n[i])
+    	{
+    		max=n[i];
+		}
+		}
+    		printf("the maximum value is %d",max);
+		
+    	
+	
+    return 0;
+}
